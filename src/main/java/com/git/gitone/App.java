4c805e6e591a2a886ecfc2c1982fa6f8744314d6
Shiva N <shivamani85939@gmail.com>
2023-05-30 14:27:03 +0530

    first commit

---
+++ src/main/java/com/git/gitone/App.java
@@ -0,0 +1,16 @@
+package com.git.gitone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public void run() {
+    	System.out.println("jdcnkdskds");
+    }
+}
